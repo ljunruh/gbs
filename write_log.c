d0037c4a7c01ee8a21381fe8523ea0849d9fdf84
Louis <tiefen.verwendet-0j@icloud.com>
2024-02-16 13:43:26 +0100

    write_log.c als Beispiel für eine Funktion, die in eine log-Datei schreibt.

---
+++ write_log.c
@@ -0,0 +1,56 @@
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <fcntl.h>
+#include <errno.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+
+// Diese Funktion schreibt eine Nachricht in eine Log-Datei im APPEND-Modus. Existiert die Datei nicht, wird sie automatisch erstellt mit den lesenden und schreibenden Rechten für den User.
+void write_log(const char *log_msg){
+    // hier aufpassen: Gänsefüßchen nehmen statt '
+    const char *log_file = "error.log";
+
+    //hier wichtig: die Flags mit einem ODER trennen und nicht mit einem Komma.
+    //0660 sind hier die Dateirechte -rw-r----- ausgewählt. Steht sonst alles in der manpage. Alternativ geht auch: S_IRWXU
+    //fd bedeutet Filedeskriptor, weil hier bitweise gelesen wird.
+    int fd = open(log_file, O_WRONLY | O_APPEND | O_CREAT, 0660);
+
+    //Weil Bitweise ausgelesen wird, gibt open() -1 zurück, wenn ein Fehler passiert ist.
+    if(fd == -1){
+        perror("Fehler bei open ");
+        return;
+    }
+
+    /*
+    Geschrieben wird hier mit ssize_t write(int fd, const void *buf, size_t count);
+    Es wird ein buffer der Länge log_msg in die Datei aus fd geschrieben.
+    write gibt die Anzahl der Bytes zurück die geschrieben werden, wenn es Fehlschlägt, dann -1.
+    Der Systemaufruf write() wird in der if-Schleife ausgeführt und dann auf Richtigkeit überprüft.
+    Weil write() keine Zeilenumbrüche verarbeiten kann muss dieser separat aufgeführt werden.
+    */
+
+    //Schreiben, wenn es nicht klappt dann Fehlermeldung ausgeben und die Datei schließen.
+    if(write(fd, log_msg, strlen(log_msg)) == -1){
+        perror("Fehler bei write");
+        close(fd);
+        return;
+    }
+
+    //Schreiben, wenn es nicht klappt dann Fehlermeldung ausgeben und die Datei schließen.
+    if(write(fd, "\n", 1) == -1){
+        perror("Fehler bei write");
+        close(fd);
+        return;
+    }
+
+    //Datei ordnungsgemäß schließen, wenn es geklappt hat.
+    close(fd);
+
+}
+
+int main(void){
+    write_log("Test-Log Nachricht");
+    return 0;
+}
